2c8f60d244a6c5977401d7db39513399a87eb11c
Szymon Królikowski <krolikowski.szymon@wp.pl>
2021-03-09 20:24:34 +0100

    FIX code

--- js/accordion.js
+++ js/accordion.js
@@ -1,7 +1,7 @@
-const accordion = document.getElementsByClassName('team__contentBox');
+const accordion = document.getElementsByClassName('changes__contentBox');
 
 for (i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener("click", function() {
-        this.classList.toggle('teamActiveBox')
+        this.classList.toggle('changesActiveBox')
     })
 }
