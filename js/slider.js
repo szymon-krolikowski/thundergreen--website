3a734348d2cf1ae05edb2506555cf6850025a8c3
Szymon Królikowski <krolikowski.szymon@wp.pl>
2021-05-22 14:30:08 +0200

    CHANGE slider

--- js/slider.js
+++ js/slider.js
@@ -1,54 +1,103 @@
-let buttonNext = document.querySelector("#next-btn");
-let slider = document.querySelector(".blog__slider");
-let buttonPrev = document.querySelector("#prev-btn");
-let flag = true;
-let i = 1;
-let percent = 100;
-let margin = 80;
-let move
+if(document.body.offsetWidth <= 700) {
+  let buttonNext = document.querySelector("#next-btn");
+  let buttonPrev = document.querySelector("#prev-btn");
+  let slider = document.querySelector(".blog__slides");
 
-const flagCheck = () => {
-  if(i === 2) {
-    flag = false; 
-  } else if(i === 0) {
-    flag = true;
-  };
-};
+  let flag = true;
+  let i = 1;
+  let percent = 100;
+  let move
 
-buttonNext.addEventListener("click", () => {
-  flagCheck();
-  if(flag == true) {
-    move = `calc(-${percent}% - ${margin}px)`;
-    slider.style.left = move;
-    slider.style.transition = "2s";
-    percent = percent + 100;
-    margin = margin + 80;
-    i++;
-  } else {
-    move = `calc(-${percent}% - ${margin}px)`;
-    slider.style.left = move;
-    slider.style.transition = "2s";
-    percent = percent - 100;
-    margin = margin - 80;
-    i--;
+  const flagCheck = () => {
+    if(i === 2) {
+      flag = false; 
+    } else if(i === 0) {
+      flag = true;
+    };
   };
-});
 
-buttonPrev.addEventListener("click", () => {
-  flagCheck();
-  if(flag == true) {
-    move = `calc(-${percent}% - ${margin}px)`;
-    slider.style.left = move;
-    slider.style.transition = "2s";
-    percent = percent - 100;
-    margin = margin - 80;
-    i++;
-  } else {
-    move = `calc(-${percent}% - ${margin}px)`;
-    slider.style.left = move;
-    slider.style.transition = "2s";
-    percent = percent + 100;
-    margin = margin + 80;
-    i--;
+  buttonNext.addEventListener("click", () => {
+    flagCheck();
+    if(flag == true) {
+      move = `calc(-${percent}%`;
+      slider.style.transform = `translateX(${move})`;
+      slider.style.transition = "2s";
+      percent = percent + 100;
+      i++;
+    } else {
+      move = `calc(-${percent}%`;
+      slider.style.transform = `translateX(${move})`;
+      slider.style.transition = "2s";
+      percent = percent - 100;
+      i--;
+    };
+  });
+
+  buttonPrev.addEventListener("click", () => {
+    flagCheck();
+    if(flag == true) {
+      move = `calc(-${percent}%`;
+      slider.style.transform = `translateX(${move})`;
+      slider.style.transition = "2s";
+      percent = percent - 100;
+      i++;
+    } else {
+      move = `calc(-${percent}%`;
+      slider.style.transform = `translateX(${move})`;
+      slider.style.transition = "2s";
+      percent = percent + 100;
+      i--;
+    };
+  });
+} else if(document.body.offsetWidth >= 701) {
+  let buttonNext = document.querySelector("#next-btn");
+  let buttonPrev = document.querySelector("#prev-btn");
+  let slider = document.querySelector(".blog__slides");
+
+  let flag = true;
+  let i = 1;
+  let percent = 50;
+  let move
+
+  const flagCheck = () => {
+    if(i === 2) {
+      flag = false; 
+    } else if(i === 0) {
+      flag = true;
+    };
   };
-});
+
+  buttonNext.addEventListener("click", () => {
+    flagCheck();
+    if(flag == true) {
+      move = `calc(-${percent}%`;
+      slider.style.transform = `translateX(${move})`;
+      slider.style.transition = "2s";
+      percent = percent + 50;
+      i++;
+    } else {
+      move = `calc(-${percent}%`;
+      slider.style.transform = `translateX(${move})`;
+      slider.style.transition = "2s";
+      percent = percent - 50;
+      i--;
+    };
+  });
+
+  buttonPrev.addEventListener("click", () => {
+    flagCheck();
+    if(flag == true) {
+      move = `calc(-${percent}%`;
+      slider.style.transform = `translateX(${move})`;
+      slider.style.transition = "2s";
+      percent = percent - 50;
+      i++;
+    } else {
+      move = `calc(-${percent}%`;
+      slider.style.transform = `translateX(${move})`;
+      slider.style.transition = "2s";
+      percent = percent + 50;
+      i--;
+    };
+  });
+}
