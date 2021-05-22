6be29d9c98f7a0c4cd9ae0039bd2f3801fc428c8
Szymon Królikowski <krolikowski.szymon@wp.pl>
2021-05-22 10:13:26 +0200

    CHANGE mobileNavigation

--- js/navigation.js
+++ js/navigation.js
@@ -1,15 +1,19 @@
 const buttonNavigationMobile = document.querySelector(".navigationMobile__button");
 const navigationMobile = document.querySelector(".navigationMobile");
-const wrapper = document.querySelector(".wrapper")
 const linksNavigationMobile = document.querySelectorAll(".navigationMobile__link")
+const line = document.querySelector(".navigationMobile__divLine");
 
 buttonNavigationMobile.addEventListener("click", () => {
     navigationMobile.classList.toggle("navigationMobileActive");
     buttonNavigationMobile.classList.toggle("navigationMobileButtonActive");
+    buttonNavigationMobile.classList.toggle("open")
 });
 
 linksNavigationMobile.forEach(element => {
     element.addEventListener("click", () => {
         navigationMobile.classList.remove("navigationMobileActive");
+        buttonNavigationMobile.classList.toggle("navigationMobileButtonActive");
+        buttonNavigationMobile.classList.toggle("open")
     });
 });
+
